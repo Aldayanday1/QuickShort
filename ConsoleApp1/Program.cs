6686bb4c112a20ae33a93ead574f759d31be596b
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-10-21 17:50:35 +0700

    Menambahkan Void Input

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace QuickShort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of data comparasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array
+        private int n;
+
+    }
+}
