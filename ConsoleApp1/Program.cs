fd262b8c69308516609539b0e08f6f6fe2c67147
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-10-21 18:20:13 +0700

    Membuat void display

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -94,6 +94,18 @@ namespace QuickShort
             //sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of data comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements:" + mov_count);
+        }
     }
 
 }
