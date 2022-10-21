edbfa1bd4c0eed8cc2a8d5d20554ed248e7e8b04
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-10-21 18:09:41 +0700

    Menambahkan Console Writeline

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,5 +12,33 @@ namespace QuickShort
         // number of elements in array
         private int n;
 
+        void input()
+        {
+            while(true)
+            {
+                Console.Write("Enter the number of element in array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 element \n");
+            }
+            Console.WriteLine("\n=======================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n=======================");
+
+            //get array elements
+            for (int i = 0;i < n;i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+                //
+            }
+
+        }
+
     }
+
 }
